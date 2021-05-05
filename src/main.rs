37c8b8727be0827e1fd09b93eeb15d61500403aa
watkazuki <watkazuki03@gmail.com>
2021-05-05 22:46:37 +0900

    initial parser

---
+++ src/main.rs
@@ -0,0 +1,13 @@
+#[macro_use]
+extern crate lalrpop_util;
+pub mod ast;
+
+lalrpop_mod!(pub html);
+
+fn main() {}
+
+#[test]
+fn html() {
+    let expr = html::HtmlParser::new().parse("<div />").unwrap();
+    assert_eq!(&format!("{}", expr), "div");
+}
