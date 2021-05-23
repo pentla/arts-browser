4ea8064388e29957f080ff244d2e126b45140e5c
watkazuki <watkazuki03@gmail.com>
2021-05-23 20:44:19 +0900

    fail: siblingのテストを追加、要素もいくつか追加

--- src/main.rs
+++ src/main.rs
@@ -26,4 +26,46 @@ fn html() {
     // spanのさらに下にtextがあるか
     assert_eq!(elem2.children[0].children.len(), 1);
     assert_eq!(elem2.children[0].children[0].name, ElementType::Text);
+
+    // 改行を含む場合
+    let test3 = html::HtmlParser::new().parse(
+        "
+<div>
+  <span>text</span>
+</div>
+",
+    );
+    assert!(test3.is_ok());
+    let elem3 = test3.unwrap();
+    assert_eq!(elem3.name, ElementType::Div);
+    // 直下にspanがあるかどうか
+    assert_eq!(elem3.children.len(), 1);
+    assert_eq!(elem3.children[0].name, ElementType::Span);
+    // spanのさらに下にtextがあるか
+    assert_eq!(elem3.children[0].children.len(), 1);
+    assert_eq!(elem3.children[0].children[0].name, ElementType::Text);
+
+    // 兄弟要素がある場合
+    let test4 = html::HtmlParser::new().parse(
+        "
+    <html>
+        <body>
+            <h1>Title</h1>
+            <div>
+                <p>Hello <em>world</em>!</p>
+            </div>
+        </body>
+    </html>
+",
+    );
+    assert!(test4.is_ok());
+    let elem4 = test4.unwrap();
+    assert_eq!(elem4.name, ElementType::HTML);
+    assert_eq!(elem4.children[0].name, ElementType::Body);
+    assert_eq!(elem4.children[0].children[0].name, ElementType::H1);
+    assert_eq!(elem4.children[0].children[1].name, ElementType::Div);
+    assert_eq!(
+        elem4.children[0].children[1].children[0].name,
+        ElementType::P
+    );
 }
