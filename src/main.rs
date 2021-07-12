deb3475d5a888f77e894de6101ff66646fd0cad2
watkazuki <watkazuki03@gmail.com>
2021-07-12 22:15:38 +0900

    pestで記述してみる

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,25 @@
+extern crate pest;
 #[macro_use]
-extern crate lalrpop_util;
+extern crate pest_derive;
 
-mod html;
+use pest::Parser;
+
+#[derive(Parser)]
+#[grammar = "html.pest"]
+pub struct HTMLParser;
 
 fn main() {}
+
+#[test]
+fn html() {
+    let parse1 = HTMLParser::parse(Rule::elementName, "a").unwrap();
+    for inner1 in parse1.into_iter() {
+        println!("{:?}", inner1);
+    }
+
+    let parse2 = HTMLParser::parse(Rule::element, "<div>").unwrap();
+    for inner2 in parse2.into_iter() {
+        println!("{:?}", inner2);
+        assert_eq!(inner2.as_str(), "div")
+    }
+}
