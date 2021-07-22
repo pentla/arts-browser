e2844f741a6ce4af2c50414354b1217354e6ca01
watkazuki <watkazuki03@gmail.com>
2021-07-22 21:57:00 +0900

    cssの宣言がparseできるように

--- src/ast_css.rs
+++ src/ast_css.rs
@@ -1,3 +1,6 @@
+use crate::ast_html::{element_type, ElementType};
+use anyhow::Result;
+
 pub struct StyleSheet {
     pub blocks: Vec<Block>,
 }
@@ -17,23 +20,39 @@ impl Block {
     pub fn set_selector(self: &mut Self, input: Selector) {
         self.selectors.push(input);
     }
+    pub fn set_declaration(self: &mut Self, declaration: Declaration) {
+        self.declarations.push(declaration);
+    }
 }
 #[derive(Debug)]
 pub struct Selector {
-    element: Option<String>,
-    class: Option<String>,
-    id: Option<String>,
-    attribute: Option<String>,
+    pub element: Option<ElementType>,
+    pub class: Option<String>,
+    pub id: Option<String>,
+    pub attribute: Option<String>,
 }
 
 impl Selector {
-    pub fn new() -> Selector {
-        Selector {
+    pub fn new(input: &str) -> Selector {
+        let mut selector = Selector {
             element: None,
             class: None,
             id: None,
             attribute: None,
+        };
+        if !input.contains(".") {
+            let first_char = input.chars().next().unwrap();
+            match first_char {
+                '.' => {
+                    selector.class = Some(input.replace(".", "").to_string());
+                }
+                '#' => {
+                    selector.id = Some(input.replace("#", "").to_string());
+                }
+                _ => selector.element = Some(element_type(input)),
+            }
         }
+        selector
     }
 }
 
@@ -43,12 +62,42 @@ pub struct Declaration {
     pub value: Value,
 }
 
-#[derive(Debug)]
+impl Declaration {
+    pub fn new(prop: &str, val: &str) -> Result<Declaration> {
+        let property = property_type(prop);
+        let mut value = Value::Undefined;
+        match property {
+            CSSProperty::Padding => {
+                if !val.contains("px") {
+                    return Err(anyhow::anyhow!("padding not px;"));
+                }
+                let remove_px = val.replace("px", "");
+                let float: f32 = remove_px.parse().unwrap();
+                value = Value::Length(float, Unit::Px);
+            }
+            _ => {}
+        }
+        Ok(Declaration { property, value })
+    }
+}
+
+#[derive(Debug, PartialEq)]
 pub enum CSSProperty {
     Color,
     BackgroundColor,
     Margin,
     Padding,
+    Undefined,
+}
+
+fn property_type(input: &str) -> CSSProperty {
+    match input {
+        "padding" => CSSProperty::Padding,
+        "margin" => CSSProperty::Margin,
+        "color" => CSSProperty::Color,
+        "background-color" => CSSProperty::BackgroundColor,
+        _ => CSSProperty::Undefined,
+    }
 }
 
 #[derive(Debug)]
@@ -56,6 +105,7 @@ pub enum Value {
     Keyword(String),
     Color,
     Length(f32, Unit),
+    Undefined,
 }
 
 #[derive(Debug)]
@@ -67,5 +117,5 @@ pub struct Color {
     r: u8,
     g: u8,
     b: u8,
-    a: u8,
+    // a: u8,
 }
--- src/parse_css.rs
+++ src/parse_css.rs
@@ -1,13 +1,16 @@
 use pest::iterators::Pair;
 use pest::Parser;
 
-use crate::ast_css::{Block, Selector};
+use crate::{
+    ast_css::{Block, CSSProperty, Declaration, Selector},
+    ast_html::ElementType,
+};
 
 #[derive(Parser)]
 #[grammar = "css.pest"]
 pub struct CSSParser;
 
-fn parse_css(input: &str) -> Block {
+fn parse_block(input: &str) -> Block {
     let parser = CSSParser::parse(Rule::block, input).unwrap();
     let mut block = Block::new();
     for line in parser.into_iter() {
@@ -23,12 +26,28 @@ fn parse_css(input: &str) -> Block {
 
 fn parse_style_block(rule: Pair<Rule>) -> Block {
     let mut block = Block::new();
-    // let selector = parse_selector(rule);
     for line in rule.into_inner().into_iter() {
         match line.as_rule() {
             Rule::selector => {
-                let selector = parse_selector(line);
+                let selector = Selector::new(line.as_str());
                 block.set_selector(selector);
+            }
+            Rule::declaration => {
+                let mut property: &str = "";
+                let mut value: &str = "";
+                for line_declaration in line.into_inner().into_iter() {
+                    match line_declaration.as_rule() {
+                        Rule::property => {
+                            property = line_declaration.as_str();
+                        }
+                        Rule::value => {
+                            value = line_declaration.as_str();
+                        }
+                        _ => {}
+                    }
+                }
+                let declaration = Declaration::new(property, value).unwrap();
+                block.set_declaration(declaration);
             }
             _ => {}
         }
@@ -36,15 +55,23 @@ fn parse_style_block(rule: Pair<Rule>) -> Block {
     block
 }
 
-fn parse_selector(rule: Pair<Rule>) -> Selector {
-    let mut selector = Selector::new();
-    println!("selector {:?}", rule);
-    selector
-}
-
 #[test]
 fn test_css_parse() {
-    let result = parse_css("a {}");
-    println!("{:?}", result);
-    assert_eq!(false, true);
+    // selectorがparseできる
+    let result1 = parse_block("a {}");
+    assert_eq!(
+        result1.selectors.get(0).unwrap().element,
+        Some(ElementType::A)
+    );
+
+    // selectorとdeclarationがparseできる
+    let result2 = parse_block("div { padding: 2px; }");
+    assert_eq!(
+        result2.selectors.get(0).unwrap().element,
+        Some(ElementType::Div)
+    );
+    assert_eq!(
+        result2.declarations.get(0).unwrap().property,
+        CSSProperty::Padding
+    );
 }
